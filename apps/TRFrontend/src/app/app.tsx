091dd33ac28eecea835566065ffe8bf7d7592033
Drimuar <Drimuar@gmail.com>
2024-06-02 16:01:46 +0700

    first commit

--- apps/TRFrontend/src/app/app.tsx
+++ apps/TRFrontend/src/app/app.tsx
@@ -1,14 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
 
-export function App() {
+
+
+export default function App() {
   return (
     <div>
-      <NxWelcome title="TRFrontend" />
+      <Layout />
     </div>
   );
 }
-
-export default App;
